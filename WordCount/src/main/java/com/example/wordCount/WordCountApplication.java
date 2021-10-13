4e1b8bfd4f0d22c159a4888db8bef69fa0472b84
Saurav Shaurya <saurav_shaurya@thbs.com>
2021-10-13 08:11:35 +0530

    added processor

--- WordCount/src/main/java/com/example/wordCount/WordCountApplication.java
+++ WordCount/src/main/java/com/example/wordCount/WordCountApplication.java
@@ -81,31 +81,24 @@ public class WordCountApplication {
 		public String getWord() {
 			return word;
 		}
-
 		public void setWord(String word) {
 			this.word = word;
 		}
-
 		public long getCount() {
 			return count;
 		}
-
 		public void setCount(long count) {
 			this.count = count;
 		}
-
 		public Date getStart() {
 			return start;
 		}
-
 		public void setStart(Date start) {
 			this.start = start;
 		}
-
 		public Date getEnd() {
 			return end;
 		}
-
 		public void setEnd(Date end) {
 			this.end = end;
 		}
